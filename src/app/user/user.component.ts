256e7d45a94bd96ca24f7ea79687c3beac88ee67
sreeja <katlasrija285@gmail.com>
2023-02-13 19:41:48 +0530

    Upto HomePage

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,30 +5,33 @@ import { MessageService } from 'primeng/api';
 import { ApiService } from '../services/api.service';
 import courseList from '../../assets/data/courseDetails.json';
 
-
-
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
-  providers:[MessageService]
+  providers: [MessageService],
 })
 export class UserComponent implements OnInit {
   contentData!: any;
-  public val: number=3;
+  public val: number = 3;
   msg: string | undefined;
 
   isLoading: boolean = false;
   public items: any;
-  public courseList: {id:number,courseImage:string,courseTitle:string,author:string,rating:number,price:number}[]=courseList;
+  public courseList: {
+    id: number;
+    courseImage: string;
+    courseTitle: string;
+    author: string;
+    rating: number;
+    price: number;
+  }[] = courseList;
 
   constructor(
     private apiService: ApiService,
     private router: Router,
     private messageService: MessageService
-  ) {
-
-  }
+  ) {}
 
   ngOnInit(): void {
     this.iconMenu();
@@ -39,30 +42,27 @@ export class UserComponent implements OnInit {
 
     //   this.val=courseList[i].rating;
     // }
-
   }
 
-
   public iconMenu(): void {
-    this.items = [{
-      label: 'Action',
-      items: [{
-        label: 'Logout',
-        icon: 'pi pi-sign-out mt-0 text-danger',
-        command: () => {
-          this.onLogout();
-        }
-      },
+    this.items = [
       {
-        label: 'Change Password',
-        icon: 'pi pi-key ',
-        command: () => {
-
-        }
-      }
-      ]
-    },
-
+        label: 'Action',
+        items: [
+          {
+            label: 'Logout',
+            icon: 'pi pi-sign-out mt-0 text-danger',
+            command: () => {
+              this.onLogout();
+            },
+          },
+          {
+            label: 'Change Password',
+            icon: 'pi pi-key ',
+            command: () => {},
+          },
+        ],
+      },
     ];
   }
 
@@ -74,18 +74,18 @@ export class UserComponent implements OnInit {
 
   public getContent(): void {
     this.isLoading = true;
-    this.apiService.getContent().subscribe(res => {
+    this.apiService.getContent().subscribe((res) => {
       try {
         this.contentData = res.data;
         console.log(this.contentData);
         this.isLoading = false;
       } catch (error) {
-        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Somethinng went to wrong !!' });
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Somethinng went to wrong !!',
+        });
       }
     });
   }
-
-
-
-
 }
