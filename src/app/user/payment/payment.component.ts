98c1b11d2be56ef57408ba0475af3f2e4fb403ab
sreeja <katlasrija285@gmail.com>
2023-02-27 18:31:24 +0530

    paymentValidations

--- src/app/user/payment/payment.component.ts
+++ src/app/user/payment/payment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-payment',
@@ -9,29 +10,61 @@ export class PaymentComponent implements OnInit{
   courseDetails!: {price:number};
   discount!:number;
   total!:number;
-  cities: { name: string; code: string; }[];
-  selectedCity1!: {};
+
+  selectedCountry :any;
+  states:any[] = [];
+
+
+  public reactiveForm!: FormGroup;
+  public country: any;
+  public state :string[]=[]
 
   ngOnInit(): void {
     this.getLocalStorageData();
+    this.reactiveForm = new FormGroup({
+
+      country: new FormControl(null, Validators.required),
+      state: new FormControl(null, Validators.required),
+    });
 
    }
-
-      constructor(){
-
-        this.cities = [
-          {name: 'New York', code: 'NY'},
-          {name: 'Rome', code: 'RM'},
-          {name: 'London', code: 'LDN'},
-          {name: 'Istanbul', code: 'IST'},
-          {name: 'Paris', code: 'PRS'}
-      ];
-      }
+  constructor(){}
   getLocalStorageData(){
     this.courseDetails = JSON.parse(localStorage.getItem('courseDetails') || '{}');
     console.log(this.courseDetails);
     this.discount=(this.courseDetails.price * 20)/100;
    this.total = this.courseDetails.price-this.discount
+  }
+
+
+ countries = [
+    { name: 'USA', code: 'usa', states: ['New York', 'California'] },
+    { name: 'Canada', code: 'canada', states: ['Ontario', 'Quebec'] },
+    { name: 'Mexico', code: 'mexico', states: ['Mexico City'] },
+    { name: 'India', code: 'India', states: ['Telangana','Andra Pradesh','Kerala','Maharastra','Goa'] }
+  ];
+sree:any
+  updateStates(event:any) {
+
+    console.log(event.target.value);
+
+    this.states=this.countries.filter( (data)=>{
+  return data.name == event.target.value
+    } )
+    console.log(this.states[0].states);
+    this.states =this.states[0].states;
+    this.state=this.states[0].states
+
+// for(let i=0;i<this.countries.length;i++){
+//   if(event.target.value === this.countries[i].name){
+//     this.states= this.countries[i].states;
+//     console.log(this.states);
+
+//   }
+// }
+  }
+  function(){
+    alert("heloo")
   }
 
 }
