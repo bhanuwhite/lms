8c2983a53189df48f4793e4d2263e2c34d0f5e6f
Shubham Kumar Das <79953451+SKD017@users.noreply.github.com>
2023-05-08 16:35:14 +0530

    Some changes in the community API

---
+++ backend/public/uploads/configdatabase_5bf06d31e1.js
@@ -0,0 +1,13 @@
+I've added the config/database.js data to .env and also pushed the code.
+
+Now working with the user_has_course and next will work on user_transaction.
+
+Ostunna means I'm coming
+Ostunnano means We're coming.
+Aante means what??
+Cheppu means say
+Vachava means Did you come??
+Poduna means morning
+Repu means tomorrow
+Eroju means today
+Sanyankalan means evening
