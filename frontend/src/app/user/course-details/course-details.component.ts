f862e6e994a796955b84aeda7a02773b1b86f01b
Mahesh <itstribe.mahi@gmail.com>
2023-05-27 22:54:09 +0530

    Working with Track API

--- frontend/src/app/user/course-details/course-details.component.ts
+++ frontend/src/app/user/course-details/course-details.component.ts
@@ -5,6 +5,7 @@ import { ApiService } from 'src/app/services/api.service';
 import { ActivatedRoute } from '@angular/router';
 import { UserLibraryGetResponseData } from 'src/app/models/user-library';
 import { MessageService } from 'primeng/api';
+import { resolve } from 'chart.js/dist/helpers/helpers.options';
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
@@ -12,7 +13,6 @@ import { MessageService } from 'primeng/api';
 })
 export class CourseDetailsComponent implements OnInit {
   streamVideo!: { url: string; name: string };
-  duration!: number;
   public userCourseData: any;
   public Spinner: boolean = true;
   activeParamId!: number;
@@ -24,7 +24,7 @@ export class CourseDetailsComponent implements OnInit {
   public trackingId: number = 0;
   public totalDurationVideo: number = 0;
   userWatchedTime: number = 1;
-  timeConsumedByUser:number = 0;
+  timeConsumedByUser: number = 0;
   @ViewChild('Course_video') Course_video: any;
 
   constructor(
@@ -33,11 +33,16 @@ export class CourseDetailsComponent implements OnInit {
     public messageService: MessageService
   ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.activeParams();
-    this.getLibraryData();
     this.getLocalStoredData();
-    this.gettingTrack();
+    // Below seconda function will execute after the 1st get completed.
+    this.getLibraryData()
+      .then(() => this.gettingTrack())
+      .then(() => this.postTrackMethod())
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   // Editing with API data
@@ -52,104 +57,110 @@ export class CourseDetailsComponent implements OnInit {
     this.globalUserId = localStoredData.id;
   }
 
-  public getLibraryData() {
-    this.apiService
-      .getSingleContentLibrary(this.activeParamId)
-      .subscribe((res) => {
-        console.log("1st console, course Res",res);
-        this.Spinner = false;
-        this.userCourseData = res.data;
-        this.courseId = this.userCourseData.attributes.course_content.data.id;
-        console.log("2nd console courseID",this.courseId);
-        this.defaultVideo();
-      });
-  }
-
-  public gettingTrack() {
-    this.apiService.getTrack().subscribe((res) => {
-      console.log('3rd Track   RESPONCE ', res);
-      this.trackResponse = res.data;
-      console.log('4th res data', this.trackResponse);
-      this.trackResponse.map((res: any) => {
-        console.log(' 5th Res OBJECT', res);
-        console.log("5.5 courseID",Number(res.attributes.course_id));
-        this.timeConsumedByUser = Math.trunc(res.attributes.time_consumed);
-        this.trackCourseIds?.push(Number(res.attributes.course_id));
-      });
-      console.log(this.trackCourseIds, '6th TRacking IDS');
-      this.postTrackMethod();
-
+  public getLibraryData(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.apiService
+        .getSingleContentLibrary(this.activeParamId)
+        .subscribe((res) => {
+          this.Spinner = false;
+          this.userCourseData = res.data;
+          this.courseId = this.userCourseData.attributes.course_content.data.id;
+          this.defaultVideo();
+          resolve();
+        }),
+        (error: any) => {
+          reject(error);
+        };
     });
   }
 
-  public postTrackMethod() {
-    const postData = {
-      data: {
-        user_id: this.globalUserId,
-        course_id: this.courseId,
-        total_duration: 0,
-        time_consumed: 0,
-      },
-    };
-    console.log(" 7th Posting Data",postData);
-
-
-    if (!this.trackCourseIds.includes(this.courseId)) {
-      console.log(" 8th this course will add");
-
-      this.apiService.postTrack(postData).subscribe((res) => {
-        try {
-          this.messageService.add({
-            severity: 'success',
-            summary: 'success',
-            detail: 'Data posted on Tracking',
-          });
-        } catch (error) {
-          console.log(error);
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Failed',
-            detail: 'Post failed',
-          });
-        }
-      });
-    }
-    else{
-      console.log(" Mahesh course already in TRACK");
-
-      this.messageService.add({
-        severity: 'error',
-        summary: 'Mahesh',
-        detail: 'This course already in TRACK API',
-      });
-    }
+  public gettingTrack(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.apiService.getTrack().subscribe((res) => {
+        this.trackResponse = res.data;
+        this.trackResponse.map((res: any) => {
+          this.timeConsumedByUser = Math.trunc(res.attributes.time_consumed);
+          this.trackCourseIds?.push(Number(res.attributes.course_id));
+        });
+        resolve();
+      }),
+        (error: any) => {
+          reject(error);
+        };
+    });
   }
 
-  userWatchedTotalTime: number = 0;
+  public postTrackMethod(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      if (this.courseId) {
+        const postData = {
+          data: {
+            user_id: this.globalUserId,
+            course_id: this.courseId,
+            total_duration: 0,
+            time_consumed: 0,
+          },
+        };
+        if (!this.trackCourseIds.includes(this.courseId)) {
+          this.apiService.postTrack(postData).subscribe((res) => {
+            try {
+              this.messageService.add({
+                severity: 'success',
+                summary: 'success',
+                detail: 'Data posted on Tracking',
+              });
+            } catch (error) {
+              console.log(error);
+              this.messageService.add({
+                severity: 'error',
+                summary: 'Failed',
+                detail: 'Post failed',
+              });
+            }
+          });
+          resolve();
+        } else {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Alert',
+            detail: 'This course already in TRACK API',
+          });
+        }
+      } else {
+        this.messageService.add({
+          severity: 'error',
+          detail: 'course Id is null check',
+        });
+      }
+      (error:any)=>{
+        reject(error)
+      }
+    });
+  }
+
   // getting Video Duration
   onMetadata(video: any) {
     this.totalDurationVideo = this.totalDurationVideo + video.duration;
-
   }
   // get running time of video.
   getWatchedTime(videoData: any) {
-  this.userWatchedTime = this.Course_video.nativeElement.currentTime;
+    this.userWatchedTime = this.Course_video.nativeElement.currentTime;
     console.log('time duration', this.userWatchedTime);
 
     const putTrackData = {
       data: {
         total_duration: Math.trunc(this.totalDurationVideo),
-        time_consumed: Math.trunc(this.userWatchedTime) + this.timeConsumedByUser,
-      }
+        time_consumed:
+          Math.trunc(this.userWatchedTime) + this.timeConsumedByUser,
+      },
     };
     console.log(putTrackData);
 
-setTimeout(() => {
-  this.apiService.putTrack(putTrackData).subscribe((res)=>{
-    console.log("Updated Track API", res);
-  })
-}, 5000);
-
+    setTimeout(() => {
+      this.apiService.putTrack(putTrackData).subscribe((res) => {
+        console.log('Updated Track API', res);
+      });
+    }, 5000);
   }
 
   // Displaying Default video 1st
