59d4a64536e7fa6146c055c61ea2e590e62edc63
Shubham Kumar Das <79953451+SKD017@users.noreply.github.com>
2023-06-05 10:56:05 +0530

    Changes for user-has-courses in User

--- backend/config/database.js
+++ backend/config/database.js
@@ -31,9 +31,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "localhost"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "lms2"),
+      database: env("DATABASE_NAME", "lms"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "root")
+      password: env("DATABASE_PASSWORD", "jammu1985")
     },
   }
 });
