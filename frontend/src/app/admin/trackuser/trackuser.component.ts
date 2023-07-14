d908a8aa292e207dc6cb8ff44600da1dc2cf93a0
Mahesh <itstribe.mahi@gmail.com>
2023-07-14 12:55:07 +0530

    some changes made in content details and track user

--- frontend/src/app/admin/trackuser/trackuser.component.ts
+++ frontend/src/app/admin/trackuser/trackuser.component.ts
@@ -20,11 +20,32 @@ export class TrackuserComponent implements OnInit, OnDestroy {
     this.getTrackApi();
   }
 
+  myUsers: any[] = [];
+
   public getTrackApi(): Promise<void> {
     return new Promise((resolve, reject) => {
       this.TrackRes$ = this.apiservivce.getTrack().subscribe((res) => {
         this.trackResponse = res.data;
       });
+
+      this.apiservivce.getAllUsers().subscribe((AllUsers) => {
+        AllUsers.map((resObj: any) => {
+          if (resObj.course_ids.length != 0 && resObj.user_id) {
+            if (this.myUsers.length === 0) {
+              this.myUsers.push(resObj);
+            } else {
+              this.myUsers.map((myUserRes: any) => {
+                if (myUserRes.user_id.id === resObj.user_id.id) {
+                  myUserRes.course_ids.push(resObj.course_ids[0]);
+                } else {
+                  this.myUsers.push(resObj);
+                }
+              });
+            }
+          }
+        });
+      });
+
       resolve();
       (err: any) => {
         reject(err);
